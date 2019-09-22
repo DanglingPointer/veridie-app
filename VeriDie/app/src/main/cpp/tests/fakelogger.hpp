48c0153c872f4940f916196df28fb81db520e25a
DanglingPointer <mikail.vasilyev@gmail.com>
2019-09-22 21:05:00 +0200

    Fixed FakeLogger and Worker unit tests

--- VeriDie/app/src/main/cpp/tests/fakelogger.hpp
+++ VeriDie/app/src/main/cpp/tests/fakelogger.hpp
@@ -1,21 +1,51 @@
 #ifndef TESTS_FAKELOGGER_HPP
 #define TESTS_FAKELOGGER_HPP
 
-#include "core/logging.hpp"
 #include <vector>
+#include <mutex>
+#include "core/logging.hpp"
 
 class FakeLogger : public ILogger
 {
 public:
-   void Write(LogPriority prio, std::string msg) override
-   {
-      entries.push_back({prio, std::move(msg)});
-   }
-
    struct Entry {
       LogPriority prio;
       std::string msg;
    };
+
+   void Write(LogPriority prio, std::string msg) override
+   {
+      std::lock_guard lg(mutex);
+      entries.push_back({prio, std::move(msg)});
+   }
+   std::vector<Entry> GetEntries() const
+   {
+      std::lock_guard lg(mutex);
+      return entries;
+   }
+   void Clear()
+   {
+      std::lock_guard lg(mutex);
+      entries.clear();
+   }
+   bool NoWarningsOrErrors() const
+   {
+      std::lock_guard lg(mutex);
+      for (auto entry : entries) {
+         switch (entry.prio) {
+            case LogPriority::ERROR:
+            case LogPriority::WARN:
+            case LogPriority::FATAL:
+               return false;
+            default:
+               break;
+         }
+      }
+      return true;
+   }
+
+private:
+   mutable std::mutex mutex;
    std::vector<Entry> entries;
 };
 
--- VeriDie/app/src/main/cpp/tests/threading_test.cpp
+++ VeriDie/app/src/main/cpp/tests/threading_test.cpp
@@ -30,23 +30,34 @@ TEST(TimerTest, timer_fires_within_1s_of_scheduled_time)
 class WorkerFixture : public ::testing::Test
 {
 protected:
-   FakeLogger logger;
+   WorkerFixture()
+      : logger(Logger())
+   {}
+   FakeLogger & logger;
+private:
+   static FakeLogger & Logger()
+   {
+      static FakeLogger logger;
+      return logger;
+   }
 };
 
 TEST_F(WorkerFixture, worker_executes_scheduled_task_within_1s_without_errors)
 {
+   logger.Clear();
    Worker w(nullptr, logger);
    std::atomic_bool done = false;
 
    w.ScheduleTask([&](auto) { done = true; });
 
    std::this_thread::sleep_for(1s);
-   EXPECT_TRUE(logger.entries.empty());
    EXPECT_TRUE(done.load());
+   EXPECT_TRUE(logger.NoWarningsOrErrors());
 }
 
 TEST_F(WorkerFixture, worker_processes_tasks_in_correct_sequence_without_errors)
 {
+   logger.Clear();
    Worker w(nullptr, logger);
    std::atomic_bool done1 = false;
    std::atomic_bool done2 = false;
@@ -57,11 +68,12 @@ TEST_F(WorkerFixture, worker_processes_tasks_in_correct_sequence_without_errors)
    while (false == done2.load())
       ;
    EXPECT_TRUE(done1);
-   EXPECT_TRUE(logger.entries.empty());
+   EXPECT_TRUE(logger.NoWarningsOrErrors());
 }
 
 TEST_F(WorkerFixture, worker_passes_its_argument_to_task)
 {
+   logger.Clear();
    int i = 42;
    Worker w(&i, logger);
    std::atomic_bool done = false;
@@ -73,6 +85,7 @@ TEST_F(WorkerFixture, worker_passes_its_argument_to_task)
    while (false == done.load())
       ;
    EXPECT_EQ((void *)(&i), actual.load());
+   EXPECT_TRUE(logger.NoWarningsOrErrors());
 }
 
 } // namespace
