884aa833a7af9cec73c543792d2d19f2c7a2b9dd
DanglingPointer <mikail.vasilyev@gmail.com>
2020-05-10 13:22:07 +0200

    Improved test for mem::internal::Deleter

--- VeriDie/app/src/main/cpp/tests/pool_test.cpp
+++ VeriDie/app/src/main/cpp/tests/pool_test.cpp
@@ -11,7 +11,7 @@ TEST(MempoolTest, mempool_shrinks_and_resizes_correctly)
       mem::Pool<2, 8, 32, 64> pool(5);
       auto p = pool.MakeUnique<std::pair<double, double>>(35.0, 36.0);
       EXPECT_EQ(5 * 4, pool.GetBlockCount());
-      EXPECT_EQ((2+8+32+64) * 5, pool.GetSize());
+      EXPECT_EQ((2 + 8 + 32 + 64) * 5, pool.GetSize());
 
       EXPECT_EQ(35.0, p->first);
       EXPECT_EQ(36.0, p->second);
@@ -27,14 +27,14 @@ TEST(MempoolTest, mempool_shrinks_and_resizes_correctly)
 
       pool.Resize(6U);
       EXPECT_EQ(6U * 4, pool.GetBlockCount());
-      EXPECT_EQ((2+8+32+64) * 6, pool.GetSize());
+      EXPECT_EQ((2 + 8 + 32 + 64) * 6, pool.GetSize());
    }
 
    {
       mem::Pool<4, 16> pool(5);
       auto p = pool.MakeUnique<int32_t>(42);
       EXPECT_EQ(5U * 2, pool.GetBlockCount());
-      EXPECT_EQ((4+16) * 5, pool.GetSize());
+      EXPECT_EQ((4 + 16) * 5, pool.GetSize());
 
       EXPECT_EQ(42, *p);
 
@@ -49,7 +49,7 @@ TEST(MempoolTest, mempool_shrinks_and_resizes_correctly)
 
       pool.Resize(6U);
       EXPECT_EQ(6U * 2, pool.GetBlockCount());
-      EXPECT_EQ((4+16) * 6, pool.GetSize());
+      EXPECT_EQ((4 + 16) * 6, pool.GetSize());
    }
 }
 
@@ -60,7 +60,7 @@ TEST(MempoolTest, mempool_shrinks_correctly_with_shared_ptr)
       auto p1 = pool.MakeShared<float>(35.0f);
       auto p2 = p1;
       EXPECT_EQ(5 * 4, pool.GetBlockCount());
-      EXPECT_EQ((2+8+32+64) * 5, pool.GetSize());
+      EXPECT_EQ((2 + 8 + 32 + 64) * 5, pool.GetSize());
       EXPECT_EQ(35.0f, *p1);
       EXPECT_EQ(35.0f, *p2);
 
@@ -84,7 +84,7 @@ TEST(MempoolTest, mempool_shrinks_correctly_with_shared_ptr)
       auto p1 = pool.MakeShared<int32_t>(42);
       auto p2 = p1;
       EXPECT_EQ(5U * 2, pool.GetBlockCount());
-      EXPECT_EQ((4+16) * 5, pool.GetSize());
+      EXPECT_EQ((4 + 16) * 5, pool.GetSize());
       EXPECT_EQ(42, *p1);
       EXPECT_EQ(42, *p2);
 
@@ -106,20 +106,17 @@ TEST(MempoolTest, mempool_shrinks_correctly_with_shared_ptr)
 
 TEST(MempoolTest, mempool_cleans_up_when_constructor_throws)
 {
-   struct MyException {};
+   struct MyException
+   {};
    struct Thrower
    {
-      Thrower() {
-         throw MyException{};
-      }
+      Thrower() { throw MyException{}; }
    };
 
    mem::Pool<2, 8, 32, 64> pool(5);
    mem::PoolPtr<Thrower> p;
 
-   EXPECT_THROW({
-      p = pool.MakeUnique<Thrower>();
-   }, MyException);
+   EXPECT_THROW({ p = pool.MakeUnique<Thrower>(); }, MyException);
 
    pool.ShrinkToFit();
    EXPECT_EQ(0U, pool.GetBlockCount());
@@ -134,14 +131,12 @@ TEST(MempoolTest, mempool_grows_when_necessary_and_calls_constructors_and_destru
    struct Counter
    {
       Counter(size_t & constructed_count, size_t & destructed_count)
-         : constructed_count(&constructed_count), destructed_count(&destructed_count)
+         : constructed_count(&constructed_count)
+         , destructed_count(&destructed_count)
       {
          (*this->constructed_count)++;
       }
-      ~Counter()
-      {
-         (*this->destructed_count)++;
-      }
+      ~Counter() { (*this->destructed_count)++; }
 
       size_t * const constructed_count;
       size_t * const destructed_count;
@@ -190,47 +185,58 @@ TEST(MempoolTest, mempool_grows_when_necessary_and_calls_constructors_and_destru
 
 TEST(MempoolTest, poolbuilder_eliminates_duplicates_and_sorts)
 {
-   using Pool = mem::PoolSuitableFor<
-      uint64_t,
-      int32_t,
-      int64_t,
-      char,
-      float,
-      uint16_t>;
+   using Pool = mem::PoolSuitableFor<uint64_t, int32_t, int64_t, char, float, uint16_t>;
 
    Pool p(1);
    EXPECT_EQ(4U, p.GetBlockCount());
-   EXPECT_EQ(1U+2U+4U+8U, p.GetSize());
+   EXPECT_EQ(1U + 2U + 4U + 8U, p.GetSize());
 }
-
-class Base
-{
-public:
-   Base() : m_i(42) {}
-   virtual ~Base() = default;
-   virtual int Get() const { return m_i; }
-
-protected:
-   int m_i;
-};
-
-class Derived : public Base
-{
-public:
-   Derived() : m_another(42) {}
-   int Get() const override { return m_i + m_another; }
-
-private:
-   int m_another;
-};
 
 TEST(MempoolTest, poolptr_handles_inheritancy_and_virtual_functions)
 {
+   class Base
+   {
+   public:
+      Base(bool * dtorCalled)
+         : m_i(42)
+         , m_destructed(dtorCalled)
+      {}
+      virtual ~Base() { *m_destructed = true; }
+      virtual int Get() const { return m_i; }
+
+   protected:
+      int m_i;
+      bool * m_destructed;
+   };
+
+   class Derived : public Base
+   {
+   public:
+      Derived(bool * childDestructed, bool * baseDestructed)
+         : Base(baseDestructed)
+         , m_another(42)
+         , m_destructed(childDestructed)
+      {}
+      ~Derived() { *m_destructed = true; }
+      int Get() const override { return m_i + m_another; }
+
+   private:
+      int m_another;
+      bool * m_destructed;
+   };
+
    using Pool = mem::PoolSuitableFor<Base, Derived>;
    Pool pool(1);
 
-   mem::PoolPtr<Base> p = pool.MakeUnique<Derived>();
-   EXPECT_EQ(42+42, p->Get());
+   bool childDtorCalled = false;
+   bool baseDtorCalled = false;
+
+   mem::PoolPtr<Base> p = pool.MakeUnique<Derived>(&childDtorCalled, &baseDtorCalled);
+   EXPECT_EQ(42 + 42, p->Get());
+
+   p.reset();
+   EXPECT_TRUE(baseDtorCalled);
+   EXPECT_TRUE(childDtorCalled);
 }
 
 } // namespace
