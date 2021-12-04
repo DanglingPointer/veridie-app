ac231d5c9050c44e76ddee4187cdab42bdba3d7e
Mikhail Vasilyev <mikail.vasilyev@gmail.com>
2021-12-04 20:36:16 +0100

    Format: split into hpp/cpp

--- VeriDie/app/src/main/cpp/utils/format.hpp
+++ VeriDie/app/src/main/cpp/utils/format.hpp
@@ -24,45 +24,13 @@ std::span<char> WriteAsText(T arg, std::span<char> dest)
    const auto [last, _] = std::to_chars(&dest[0], &dest[dest.size()], arg);
    return dest.subspan(std::distance(dest.data(), last));
 }
-inline std::span<char> WriteAsText(char arg, std::span<char> dest)
-{
-   assert(!dest.empty());
-   dest[0] = arg;
-   return dest.subspan(1);
-}
-inline std::span<char> WriteAsText(std::string_view arg, std::span<char> dest)
-{
-   assert(!dest.empty());
-   const size_t length = std::min(arg.size(), dest.size());
-   std::copy_n(arg.cbegin(), length, dest.begin());
-   return dest.subspan(length);
-}
-inline std::span<char> WriteAsText(void * arg, std::span<char> dest)
-{
-   assert(!dest.empty());
-   static constexpr std::string_view prefix = "0x";
-   dest = WriteAsText(prefix, dest);
-   const auto [last, _] =
-      std::to_chars(&dest[0], &dest[dest.size()], reinterpret_cast<uintptr_t>(arg), 16);
-   return dest.subspan(std::distance(dest.data(), last));
-}
-inline std::span<char> WriteAsText(bool arg, std::span<char> dest)
-{
-   assert(!dest.empty());
-   static constexpr std::string_view t = "true";
-   static constexpr std::string_view f = "false";
-   return arg ? WriteAsText(t, dest) : WriteAsText(f, dest);
-}
-inline std::span<char> WriteAsText(const char * arg, std::span<char> dest)
-{
-   assert(!dest.empty());
-   return WriteAsText(std::string_view(arg), dest);
-}
-inline std::span<char> WriteAsText(const std::string & arg, std::span<char> dest)
-{
-   assert(!dest.empty());
-   return WriteAsText(std::string_view(arg), dest);
-}
+std::span<char> WriteAsText(char arg, std::span<char> dest);
+std::span<char> WriteAsText(std::string_view arg, std::span<char> dest);
+std::span<char> WriteAsText(void * arg, std::span<char> dest);
+std::span<char> WriteAsText(bool arg, std::span<char> dest);
+std::span<char> WriteAsText(const char * arg, std::span<char> dest);
+std::span<char> WriteAsText(const std::string & arg, std::span<char> dest);
+
 // clang-format off
 template <typename T>
 concept Writable = requires(T && arg, std::span<char> buf) {
@@ -70,33 +38,10 @@ concept Writable = requires(T && arg, std::span<char> buf) {
 };
 // clang-format on
 
-inline size_t ParsePlaceholder(std::string_view from)
-{
-   static constexpr std::string_view placeholder = "{}";
-   return from.starts_with(placeholder) ? placeholder.size() : 0;
-}
-inline size_t CountPlaceholders(std::string_view fmt)
-{
-   size_t count = 0;
-   for (size_t i = 0; i < fmt.size(); ++i)
-      count += ParsePlaceholder(fmt.substr(i)) ? 1 : 0;
-   return count;
-}
-inline std::tuple<std::string_view, std::span<char>> CopyUntilPlaceholder(std::string_view src,
-                                                                          std::span<char> dest)
-{
-   size_t srcPos = 0;
-   size_t destPos = 0;
-   while (srcPos < src.size() && destPos < dest.size()) {
-      const size_t placeholderLength = ParsePlaceholder(src.substr(srcPos));
-      if (placeholderLength > 0) {
-         srcPos += placeholderLength;
-         break;
-      }
-      dest[destPos++] = src[srcPos++];
-   }
-   return {src.substr(srcPos), dest.subspan(destPos)};
-}
+size_t ParsePlaceholder(std::string_view from);
+size_t CountPlaceholders(std::string_view fmt);
+std::tuple<std::string_view, std::span<char>> CopyUntilPlaceholder(std::string_view src,
+                                                                   std::span<char> dest);
 
 } // namespace internal
 
